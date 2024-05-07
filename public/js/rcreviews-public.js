6078eb556b1c291e81fde8627a3838504da80476
juliusfromsl <julius@stafflink.com.au>
2024-05-07 11:44:26 +0800

    Revision to truncate script.

--- public/js/rcreviews-public.js
+++ public/js/rcreviews-public.js
@@ -1,62 +1,47 @@
 jQuery( document ).ready(function($) {
 	'use strict';
 
-	/**
-	 * All of the code for your public-facing JavaScript source
-	 * should reside in this file.
-	 *
-	 * Note: It has been assumed you will write jQuery code here, so the
-	 * $ function reference has been prepared for usage within the scope
-	 * of this function.
-	 *
-	 * This enables you to define handlers, for when the DOM is ready:
-	 *
-	 * $(function() {
-	 *
-	 * });
-	 *
-	 * When the window is loaded:
-	 *
-	 * $( window ).load(function() {
-	 *
-	 * });
-	 *
-	 * ...and/or other possibilities.
-	 *
-	 * Ideally, it is not considered best practise to attach more than a
-	 * single DOM-ready or window-load handler for a particular page.
-	 * Although scripts in the WordPress core, Plugins and Themes may be
-	 * practising this, we should strive to set a better example in our own work.
-	 */
-
 	var $icon_down  = '<svg class="CK__Icon--medium" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" width="16" height="16"><path fill-rule="evenodd" clip-rule="evenodd" d="M1.146 4.646a.5.5 0 0 0 0 .708l6.5 6.5a.5.5 0 0 0 .708 0l6.5-6.5a.5.5 0 0 0-.708-.708L8 10.793 1.854 4.646a.5.5 0 0 0-.708 0Z" fill="#00639E"></path></svg>';
 
-	$('.rcreviews--content').each(function() {
-		var $this = $(this);
-		var lineHeight = parseInt($this.css('line-height'), 10);
-		var lines = Math.ceil($this[0].scrollHeight / lineHeight);
-		
-		if ( 2 < lines ){
-			$this.addClass('rcreviews--truncate');
-			$this.after('<div class="rcreviews--read-more-wrapper"><span class="rcreviews--read-more">Read More ' + $icon_down + '</span></div>');
-		}
-	});
-
-	$('.rcreviews--read-more').on('click', function() {
-		$(this).parent().prev().removeClass('rcreviews--truncate');
-		$(this).remove();
-	});
+	function truncateContent() {
+		$('.rcreviews--content').each(function() {
+			var $this = $(this);
+			var lineHeight = parseInt($this.css('line-height'), 10);
+			var lines = Math.ceil($this[0].scrollHeight / lineHeight);
+			
+			if ( 2 < lines && !$this.hasClass('rcreviews--has-truncate') ){
+				$this.addClass('rcreviews--has-truncate');
+				$this.addClass('rcreviews--truncate');
+				$this.after('<div class="rcreviews--read-more-wrapper"><span class="rcreviews--read-more">Read More ' + $icon_down + '</span></div>');
+			}
+		});
+	}
+	
+	truncateContent();
 
 	$('.rcreviews--btn').on('click', function() {
-		$('.rcreviews--hidden-review').toggleClass('d-none');
-		$('.rcreviews--label').toggleClass('active');
-		$('.rcreviews--count').toggleClass('d-none');
+		$('.rcreviews--hidden-review').each(function() {
+			$(this).toggleClass('d-none');
+		});
+		
+		truncateContent();
 
-		if ( $('.rcreviews--label').hasClass('active') ){
-			$('.rcreviews--label').text('Show less');
-		} else{
-			$('.rcreviews--label').text('Show');
+		var label = $('.rcreviews--label');
+		var count = $('.rcreviews--count');
+		label.toggleClass('active');
+		count.toggleClass('d-none');
+	
+		if (label.hasClass('active')) {
+			label.text('Show less');
+		} else {
+			label.text('Show');
 		}
+	});
+
+	$(document).on('click', '.rcreviews--read-more', function() {
+		$(this).parent().prev().removeClass('rcreviews--truncate');
+		$(this).parent().remove();
+		console.log('click');
 	});
 
 });
