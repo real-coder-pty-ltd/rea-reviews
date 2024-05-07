cea2dc68c870bc1c35dd25eee183be2358ed44d1
juliusfromsl <julius@stafflink.com.au>
2024-05-07 08:10:38 +0800

    Updates on content to add truncate and read more button to expand the copy.

--- public/js/rcreviews-public.js
+++ public/js/rcreviews-public.js
@@ -1,4 +1,4 @@
-(function( $ ) {
+jQuery( document ).ready(function($) {
 	'use strict';
 
 	/**
@@ -29,4 +29,24 @@
 	 * practising this, we should strive to set a better example in our own work.
 	 */
 
-})( jQuery );
+	var $icon_down  = '<svg class="CK__Icon--medium" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" width="16" height="16"><path fill-rule="evenodd" clip-rule="evenodd" d="M1.146 4.646a.5.5 0 0 0 0 .708l6.5 6.5a.5.5 0 0 0 .708 0l6.5-6.5a.5.5 0 0 0-.708-.708L8 10.793 1.854 4.646a.5.5 0 0 0-.708 0Z" fill="#00639E"></path></svg>';
+
+	$('.rcreviews-content').each(function() {
+		var $this = $(this);
+		var lineHeight = parseInt($this.css('line-height'), 10);
+		var lines = Math.ceil($this[0].scrollHeight / lineHeight);
+		
+		if ( 2 < lines ){
+			$this.addClass('truncate');
+			$this.after('<div class="read-more-wrapper"><span class="read-more">Read More ' + $icon_down + '</span></div>');
+		}
+	});
+
+	$('.read-more').on('click', function() {
+		$(this).parent().prev().removeClass('truncate');
+		$(this).remove();
+	});
+
+});
+
+
